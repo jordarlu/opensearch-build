de24dd37b707211921df1ce618ab4599315be0f4
Jeff Lu <chunglu@amazon.com>
2024-04-09 12:59:24 -0700

    Update gradle-check.sh
    Signed-off-by: Jeff Lu <chunglu@amazon.com>

--- scripts/gradle/gradle-check.sh
+++ scripts/gradle/gradle-check.sh
@@ -12,7 +12,7 @@
 # To trigger Jenkins Gradle Check from a PR
 
 
-JENKINS_URL="https://ci-beta.opensearch.org"
+JENKINS_URL="https://ci-beta.opensearch.org/gradle/"
 TIMEPASS=0
 TIMEOUT=7200
 RESULT="null"
